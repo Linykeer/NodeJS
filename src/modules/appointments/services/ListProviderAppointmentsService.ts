8cdb20bbbc5cddd47131b2b3e2cfeb2849c49be6
Linykeer Almeida <linykeerrenato@gmail.com>
2020-09-30 15:08:37 -0300

    Finish

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,51 @@
+import { injectable, inject } from 'tsyringe';
+
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+  day: number;
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IRequest): Promise<Appointment[]> {
+    const cachKey = `provider-appointments:${provider_id}-${year}-${month}-${day}`;
+
+    let appointments = await this.cacheProvider.recover<Appointment[]>(cachKey);
+
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+        {
+          provider_id,
+          year,
+          month,
+          day,
+        },
+      );
+
+      await this.cacheProvider.save(cachKey, appointments);
+    }
+
+    return appointments;
+  }
+}
+
+export default ListProviderAppointmentsService;
---
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,12 @@
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
+import ICreateUserDTO from '../dtos/ICreateUserDTO';
+
+import User from '../infra/typeorm/entities/User';
+
+export default interface IUsersRepository {
+  save(user: User): Promise<User>;
+  create(data: ICreateUserDTO): Promise<User>;
+  findById(id: string): Promise<User | undefined>;
+  findByEmail(email: string): Promise<User | undefined>;
+  findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
+}
